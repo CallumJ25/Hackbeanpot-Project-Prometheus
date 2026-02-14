b747931ce382e9b61768fb364c0e704c64bf71a5
Naman Patel <patel.naman3@gmail.com>
2026-02-14 17:33:13 -0500

    login button that is broken

--- src/PageLayout.jsx
+++ src/PageLayout.jsx
@@ -149,6 +149,16 @@ export const TopNav = ({ soundEnabled, setSoundEnabled, totalCorrect, totalAnswe
 
       {/* Settings - top right */}
       <div className="fixed top-4 right-4 z-50 flex items-center gap-2 md:right-20">
+        <button
+          onClick={resetProgress}
+          >
+          <Link 
+            to="/module/1"
+            className="bg-white/90 backdrop-blur px-10 py-2 rounded-full shadow-lg hover:bg-white transition-colors text-navy-light text-center"
+          >
+            Log In
+          </Link>
+        </button>
         {totalAnswered > 0 && (
           <div className="bg-white/90 backdrop-blur px-3 py-2 rounded-full shadow-lg text-sm text-navy">
             {totalCorrect}/{totalAnswered}
